35a853cd13665ca47111a6bac4267c2e0be77374
David Andres Noreña Lara <norenad@uninorte.edu.co>
2025-09-29 09:27:18 -0500

    Feat: Added classes and atributes

---
+++ src/core/Cliente.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author norenad
+ */
+public class Cliente {
+private String nombre;
+private ArrayList<Pedido>pedidos=new ArrayList();
+
+    public Cliente(String nombre) {
+        this.nombre = nombre;
+        
+    }
+   
+}
